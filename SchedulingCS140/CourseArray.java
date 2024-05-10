e1d11194dadfc1f30c1fc2de1f00c7de2a2c89ab
Lana <112628962+svetlanatad@users.noreply.github.com>
2024-05-10 14:12:05 +0400

    added getTimeSlot

--- SchedulingCS140/CourseArray.java
+++ SchedulingCS140/CourseArray.java
@@ -90,4 +90,19 @@ public class CourseArray {
 		for (int i = 1; i < elements.length; i++)
 			System.out.println(i + "\t" + elements[i].mySlot);
 	}
+
+     public int[] getTimeSlot(int index) {
+        int[] timeSlot = new int[elements.length];
+        
+        for (int i = 1; i < elements.length; i++) {
+        
+            if (elements[i].mySlot == index) {
+                timeSlot[i] = 1;
+            } else {
+                timeSlot[i] = -1;
+            }
+        }
+        
+        return timeSlot;
+    }
 }
